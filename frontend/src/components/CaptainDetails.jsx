0d661de7a91fe8ad01b621e1abb94c535721a289
Sana perween <sonukhan.doranda@gmail.com>
2026-03-09 19:46:03 +0530

    added

--- frontend/src/components/CaptainDetails.jsx
+++ frontend/src/components/CaptainDetails.jsx
@@ -12,8 +12,8 @@ const CaptainDetails = () => {
        <div className='flex items-center justify-between'>
         <div className='flex items-center justify-start gap-3'>
           <img className= 'h-10 w-10 rounded-full object-cover' src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
-          <h4 className='text-lg font-medium capitalize'>{captain?.fullname?.firstname}
-            {captain?.fullname?.lastname}</h4>
+          <h4 className='text-lg font-medium capitalize'>{ride?.captain?.fullname?.firstname}
+            {ride?.captain?.fullname?.lastname}</h4>
         </div>
         <div>
           <h4 className='text-xl font-semibold'>₹295.30</h4>
