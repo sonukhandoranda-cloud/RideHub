a31284e5afee07a1d47e858e2733099c0b867116
Sana perween <sonukhan.doranda@gmail.com>
2026-03-09 19:37:30 +0530

    fix socket connection

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 export const SocketContext = createContext(null);
 
 
-const socket = io( "http://localhost:4000",
+const socket = io( import.meta.env.VITE_BASE_URL,
   {
     transports:["websocket"], });
 
